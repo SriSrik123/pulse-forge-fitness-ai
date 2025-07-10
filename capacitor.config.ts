3dfa0a02484d00424588a62b4917890a6465c10b
ss5827 <hellosri2006@gmail.com>
2025-07-10 12:25:41 +0530

    Small tweaks

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,11 @@
 
-import { CapacitorConfig } from '@capacitor/core';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.lovable.a0147a6942cb4db7a4a4a13287c24c96',
   appName: 'PulseTrack',
   webDir: 'dist',
-  server: {
-    url: 'https://a0147a69-42cb-4db7-a4a4-a13287c24c96.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+
   plugins: {
     SplashScreen: {
       launchShowDuration: 2000,
