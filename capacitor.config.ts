a778c63ed026f7f8791c2118d215098921fdda51
ss5827 <hellosri2006@gmail.com>
2025-07-11 18:34:18 +0530

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.yourcompany.pulsetrack', // <--- Change this to your desired ID
+  appId: 'com.yourcompany.pulsetrack',
   appName: 'PulseTrack',
   webDir: 'dist',
   plugins: {
