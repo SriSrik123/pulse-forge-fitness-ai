d385f47f5607ee75c675617df43dc755db7f5c91
ss5827 <hellosri2006@gmail.com>
2025-07-16 09:16:51 +0530

    Changed website opening

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,10 +5,6 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.a0147a6942cb4db7a4a4a13287c24c96',
   appName: 'CoachMe',
   webDir: 'dist',
-  server: {
-    url: 'https://a0147a69-42cb-4db7-a4a4-a13287c24c96.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
   plugins: {
     SamsungHealth: {
       appId: 'com.yourcompany.pulsetrack'
